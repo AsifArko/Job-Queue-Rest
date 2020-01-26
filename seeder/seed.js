b268b003c2206ae82952453b5070a3932690b656
AsifArko <asifarko97@gmail.com>
2020-01-26 14:32:22 +0600

    rfmt

--- seeder/seed.js
+++ seeder/seed.js
@@ -16,4 +16,16 @@ async function seed() {
     await workQueue.add(data)
 }
 
-setInterval(seed, 1000);
+async function seedWithDelay() {
+    let data = {
+        name: "SEND_EMAIL",
+        email: "asif@kickbackapps.com",
+        message: Math.random().toString(36).substring(7),
+    };
+
+    console.log(`Pushing Job ${JSON.stringify(data)}`);
+    await workQueue.add(data, {delay: 5000})
+}
+
+setInterval(seed, 10);
+setInterval(seedWithDelay, 5);
