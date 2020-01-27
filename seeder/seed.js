d01c272972b08878fc9cdc684d34260af1c7387a
AsifArko <asifarko97@gmail.com>
2020-01-27 12:30:57 +0600

    modification

--- seeder/seed.js
+++ seeder/seed.js
@@ -11,7 +11,6 @@ async function seed() {
         email: "asif@kickbackapps.com",
         message: Math.random().toString(36).substring(7),
     };
-
     console.log(`Pushing Job ${JSON.stringify(data)}`);
     await workQueue.add(data)
 }
@@ -22,10 +21,20 @@ async function seedWithDelay() {
         email: "asif@kickbackapps.com",
         message: Math.random().toString(36).substring(7),
     };
-
     console.log(`Pushing Job ${JSON.stringify(data)}`);
-    await workQueue.add(data, {delay: 5000})
+    await workQueue.add(data, {delay: Math.floor(Math.random()*10000)})
 }
 
-setInterval(seed, 10);
-setInterval(seedWithDelay, 5);
+async function seedWithPriority() {
+    let data = {
+        name: "SEND_EMAIL",
+        email: "asif@kickbackapps.com",
+        message: "Priority test",
+    };
+    console.log(`Pushing Job ${JSON.stringify(data)}`);
+    await workQueue.add(data, {priority: 1})
+}
+
+setInterval(seed, Math.floor(Math.random()*10000));
+setInterval(seedWithDelay, Math.floor(Math.random()*10000));
+// setInterval(seedWithPriority, Math.floor(Math.random()*10000));
